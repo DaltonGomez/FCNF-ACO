32b6ec077770cd23c1daa1749be1a79a7c2eb77a
Dalton Gomez <69910162+DaltonGomez@users.noreply.github.com>
2022-04-24 19:22:31 -0600

    Adding networks for running the results experiment in triplicate

--- src/Network/make_network_script.py
+++ src/Network/make_network_script.py
@@ -1,9 +1,9 @@
 from src.Network.GraphMaker import GraphMaker
 
-name = " "
-numNodes = 10
-numSources = 1
-numSinks = 1
+name = "400-1-10-3"
+numNodes = 400
+numSources = 10
+numSinks = 10
 
 graphMaker = GraphMaker(name, numNodes, numSources, numSinks)
 # Uncomment to tune how the network generates costs and to turn on generalizations
