7fc24702c7bfa2a8323688160b910f5be6b7b27a
Dalton Gomez <69910162+DaltonGomez@users.noreply.github.com>
2022-04-30 17:25:16 -0600

    Adds additional results CSV

--- src/Experiments/batch_experiment_script.py
+++ src/Experiments/batch_experiment_script.py
@@ -4,9 +4,9 @@ import random
 from src.Experiments.ResultsExperiment import ResultsExperiment
 from src.Network.GraphMaker import GraphMaker
 
-numGraphs = 10
+numGraphs = 50
 nodeSizeRange = [25, 400]
-srcSinkRange = [1, 10]
+srcSinkSet = [1, 5, 10]
 possibleArcCaps = [100]
 networkList = []
 
@@ -15,11 +15,11 @@ for n in range(numGraphs):
     random.seed()
     # Uniformly sample number of nodes
     numNodes = random.randint(nodeSizeRange[0], nodeSizeRange[1])
-    numSrcSinks = random.randint(srcSinkRange[0], srcSinkRange[1])
+    numSrcSinks = random.sample(srcSinkSet, 1)[0]
     # Keep sampling until there is enough nodes to support the sources and sinks
     while numSrcSinks > math.floor(numNodes / 2):
         numNodes = random.randint(nodeSizeRange[0], nodeSizeRange[1])
-        numSrcSinks = random.randint(srcSinkRange[0], srcSinkRange[1])
+        numSrcSinks = random.randint(srcSinkSet[0], srcSinkSet[1])
     # Build and save the network
     networkName = str(numNodes) + "-" + str(len(possibleArcCaps)) + "-" + str(numSrcSinks) + "-" + str(n)
     graphMaker = GraphMaker(networkName, numNodes, numSrcSinks, numSrcSinks)
